f6df7e7f100ba35bbac4e63f4bfbc222f4b6b7be
TrieuDuong <ntduong1998vn@gmail.com>
2020-04-09 19:18:37 +0700

    Updates

---
+++ precache-manifest.08b7763e81d54aeeb77a224ff0041373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcf4062f173795b8644e70802c6bf69",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "3f283aa78b487bb4b0a9",
+    "url": "/covid-19-tracker/static/css/main.4720bfe4.chunk.css"
+  },
+  {
+    "revision": "bea5564449b5df5cffd0",
+    "url": "/covid-19-tracker/static/js/2.706b6b0a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.706b6b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f283aa78b487bb4b0a9",
+    "url": "/covid-19-tracker/static/js/main.9dae718b.chunk.js"
+  },
+  {
+    "revision": "6f90f5df408e067b3b09",
+    "url": "/covid-19-tracker/static/js/runtime-main.c4aad412.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
